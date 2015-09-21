39e2c7b3a55bcb571303ea32ea6b761fd659444a
Fionnuala Cavanagh <f.c.cavanagh@gmail.com>
2015-09-21 14:54:16 +0100

    Update rockpaperscissors.js
    up to unit 4 homework

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -31,14 +31,23 @@ function getComputerMove(move) {
 
 git commit -m "unit 3 homework is above"
 
+getWinner(playerMove)
 function getWinner(playerMove,computerMove) {
     var winner;
-    // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
-    // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
-    // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+    
+    if (playerMove === computerMove){
+            winner = 'tie';
+    } else if (playerMove === 'rock' && computerMove === 'scissors') {
+            winner = 'player';
+    } else if (playerMove === 'paper' && computerMove === 'scissors') {
+            winner = 'computer';
+    } else if (playerMove === 'paper' && computerMove === 'rock') {
+            winner = 'computer';
+    }
     return winner;
 }
+
+git commit -m "unit 4 homework is above"
 
 function playToFive() {
     console.log("Let's play Rock, Paper, Scissors");
